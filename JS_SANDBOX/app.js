63a8db3be934e63a49e8357cc1214f841b983566
Ashim Gelal <gelalasim@gmail.com>
2021-07-14 00:58:41 +0545

    Traversy Web_dev
    Praticing the webdevelpment traversy udemny course

---
+++ JS_SANDBOX/app.js
@@ -0,0 +1,26 @@
+// // Log to console
+// console.log("Hello World");
+// console.log(123);
+// console.log(true);
+
+// var greeting = 'Hello';
+// console.log(greeting);
+// console.log(1, 2, 3, 5);
+// console.log({ a: 1, b: 2 });
+// console.table({ a: 1, b: 2 });
+
+// console.error("There is some error");
+// // console.clear();
+// console.warn("THis is warning");
+// console.time("Hello");
+// console.log('hello World');
+// console.log("Hello World");
+// console.log("Hello World");
+// console.timeEnd("Hello");
+
+// var, let, const
+var name = 'John Doe';
+console.log(name);
+
+name = 'Steve Smith';
+console.log(name);
